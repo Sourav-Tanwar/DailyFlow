b26a665ac2c1bdb95278c9ac49b5a3527707fa7d
Sourav-Tanwar <souravtnw@gmail.com>
2025-04-24 21:41:36 +0530

    Add Expense Button

--- frontend/Dailyflow/src/components/ExpenseTracker.jsx
+++ frontend/Dailyflow/src/components/ExpenseTracker.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 const ExpenseTracker = () => {
   return (
     <>
-      
-      
       <div className="bg-gray-100">
         <div className="max-w-6xl mx-auto p-6">
           <h1 className="text-3xl font-bold mb-6">Expense Dashboard</h1>
+          <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+              Add Expense
+            </span>
+          </button>
 
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             <div className="bg-white rounded-xl shadow p-6">
