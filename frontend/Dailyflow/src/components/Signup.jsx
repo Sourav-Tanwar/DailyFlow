ce91251d37e3b8396c241021bde3a2ade0a795c1
Sourav-Tanwar <souravtnw@gmail.com>
2025-06-03 16:55:10 +0530

    Darkmode issue fixed on Login and Signup

--- frontend/Dailyflow/src/components/Signup.jsx
+++ frontend/Dailyflow/src/components/Signup.jsx
@@ -62,22 +62,22 @@ export default function Login() {
       <div className="container">
         <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
           <div className="mb-5">
-            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-            <input type="text" id="name" name="name" value={newUser.name} onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Name</label>
+            <input type="text" id="name" name="name" value={newUser.name} onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   dark:text-white  " required />
           </div>
           <div className="mb-5">
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
-            <input type="email" id="email" name="email" value={newUser.email} onChange={onChange} autoComplete="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
-          </div>
-
-          <div className="mb-5">
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-            <input type="password" id="password" name='password' value={newUser.password} onChange={onChange} autoComplete="new-password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Email address</label>
+            <input type="email" id="email" name="email" value={newUser.email} onChange={onChange} autoComplete="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="example@xyz.com" required />
           </div>
 
           <div className="mb-5">
-            <label htmlFor="confirmPass" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" >Confirm Password</label>
-            <input type="password" id="confirmPass" name='confirmPass' value={newUser.confirmPass} onChange={onChange} autoComplete="new-password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Password</label>
+            <input type="password" id="password" name='password' value={newUser.password} onChange={onChange} autoComplete="new-password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required />
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="confirmPass" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900" >Confirm Password</label>
+            <input type="password" id="confirmPass" name='confirmPass' value={newUser.confirmPass} onChange={onChange} autoComplete="new-password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
           </div>
           <button type="submit" disabled={loading} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{loading ? "Signing in..." : "Submit"}</button>
           <Link to="/login" className="m-3 btn btn-danger">Already signed up.</Link>
--- frontend/Dailyflow/tailwind.config.js
+++ frontend/Dailyflow/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
+  darkMode: false,
   theme: {
     extend: {},
   },
