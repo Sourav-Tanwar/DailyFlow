de8bfc7a61e83e3421544c5d6fd6c19a11e4c568
Sourav-Tanwar <souravtnw@gmail.com>
2026-01-23 11:17:23 +0530

    SignUp Name text color issue fixed

--- frontend/Dailyflow/src/components/Signup.jsx
+++ frontend/Dailyflow/src/components/Signup.jsx
@@ -63,7 +63,7 @@ export default function Login() {
         <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
           <div className="mb-5">
             <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Name</label>
-            <input type="text" id="name" name="name" value={newUser.name} onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   dark:text-white  " required />
+            <input type="text" id="name" name="name" value={newUser.name} onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
           </div>
           <div className="mb-5">
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Email address</label>
